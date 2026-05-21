9ba940c355b95b4764bb0c75b420b35158900627
jeffersonveloz-lab <jeffersonvelozh@gmail.com>
2026-05-21 16:23:45 -0500

    pequeños cambios

--- Leccion-Practica-2/src/app/features/pages/privated/dashboard-component/dashboard-component.ts
+++ Leccion-Practica-2/src/app/features/pages/privated/dashboard-component/dashboard-component.ts
@@ -21,7 +21,6 @@ export class DashboardComponent {
 
   stats: StatCard[] = [
     { icon: 'shopping_bag', label: 'Productos', value: '30', color: '#1976d2' },
-    { icon: 'people', label: 'Usuarios', value: '2', color: '#388e3c' },
     { icon: 'receipt_long', label: 'Órdenes', value: '7', color: '#f57c00' },
     { icon: 'attach_money', label: 'Ventas del mes', value: '$1,240', color: '#7b1fa2' },
   ];
